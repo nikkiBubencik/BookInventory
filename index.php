bf275147ed6fd9436dae5efd70a3c00c3ee7a87d
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-18 17:30:06 -0400

    Update index.php
    changed logo

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
 		<header>
 			<div class="header-left">
 				<div class="logo">
-					<img src="imgs/logo.png" alt="Toy R URI Logo">
+					<img src="imgs/book-logo.jpeg" alt="Toy R URI Logo">
       			</div>
 
 	      		<nav>
