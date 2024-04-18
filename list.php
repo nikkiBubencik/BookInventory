d7968c3cb56c248cb98faa487786ba8f9687c292
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-18 18:38:05 -0400

    Update list.php
    modified list.php to show title and authors for all books in chosen list

--- list.php
+++ list.php
@@ -2,7 +2,7 @@
 	
 	// Include the database connection script
 	require 'includes/database-connection.php';
-
+	$listID = $_GET['listID'];
 	/*
 	 * Retrieve toy information from the database based on the toy ID.
 	 * 
@@ -10,37 +10,23 @@
 	 * @param string $id     The ID of the toy to retrieve.
 	 * @return array|null    An associative array containing the toy information, or null if no toy is found.
 	 */
-	function get_book(PDO $pdo, string $id) {
+	function get_books(PDO $pdo, string $id) {
 
-		// SQL query to retrieve book information based on the book ID
-		$sql = "SELECT * 
-			FROM books
-			WHERE bookID= :id;";	// :id is a placeholder for value provided later 
+		// SQL query to retrieve books in a list
+		$sql = "SELECT b.title, b.authors
+			FROM books as b JOIN user_books as ub ON b.bookID = ub.bookID
+			WHERE listID= :id;";	// :id is a placeholder for value provided later 
 		                               // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
 
 
 		// Execute the SQL query using the pdo function and fetch the result
-		$book = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+		$book = pdo($pdo, $sql, ['id' => $id])->fetchAll();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
 
-		// Return the toy information (associative array)
+		// Return the book information (associative array)
 		return $book;
 	}
 
-	// Retrieve info about toy with ID '0001' from the db using provided PDO connection
-	$book1 = get_book($pdo, '2');
-	// $toy2 = get_toy($pdo, '0002');
-	// $toy3 = get_toy($pdo, '0003');
-	// $toy4 = get_toy($pdo, '0004');
-	// $toy5 = get_toy($pdo, '0005');
-	// $toy6 = get_toy($pdo, '0006');
-	// $toy7 = get_toy($pdo, '0007');
-	// $toy8 = get_toy($pdo, '0008');
-	// $toy9 = get_toy($pdo, '0009');
-	// $toy10 = get_toy($pdo, '0010');
-	/*
-	 * TO-DO: Retrieve info for ALL remaining toys from the db
-	 */
-
+	$allBooks = get_book($pdo, listID);
 
 // Closing PHP tag  ?> 
 
@@ -50,7 +36,7 @@
 	<head>
 		<meta charset="UTF-8">
   		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  		<title>Toys R URI</title>
+  		<title>Book Inventory</title>
   		<link rel="stylesheet" href="css/style.css">
   		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -62,7 +48,7 @@
 		<header>
 			<div class="header-left">
 				<div class="logo">
-					<img src="imgs/book-logo.jpg" alt="Toy R URI Logo">
+					<img src="imgs/book-logo.jpg" alt="Book Inventory Logo">
       			</div>
 
 	      		<nav>
@@ -81,19 +67,20 @@
 		</header>
 
   		<main>
-  			<section class="toy-catalog">
-
-  				<div class="toy-card">
-  					<!-- Create a hyperlink to toy.php page with toy number as parameter -->
-
-  					<!-- Displaytitle of book -->
-					<a href="book.php?bookID=<?= $book1['bookID'] ?>">
-  						<h2><?= $book1['title'] ?></h2>
-
-  					<!-- Display authors -->
-  					<p><?= $book1['authors'] ?></p>
-					</a>
-  				</div>
+  			<section class="book-catalog">
+				<?php foreach ($allBooks as $book): ?>
+	  				<div class="book-card">
+	  					<!-- Create a hyperlink to book.php page with book number as parameter -->
+	
+	  					<!-- Displaytitle of book -->
+						<a href="book.php?bookID=<?= $book['bookID'] ?>">
+	  						<h2><?= $book['title'] ?></h2>
+	
+	  					<!-- Display authors -->
+	  					<p><?= $book['authors'] ?></p>
+						</a>
+	  				</div>
+				<?php endforeach; ?>
 
    			</section>
   		</main>
