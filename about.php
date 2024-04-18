db8337ec565ba34cc43b31c137f3d0450cf8a1ff
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-18 17:38:14 -0400

    Update about.php
    changed to books should update about portion later

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="UTF-8">
   		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  		<title>Toys R URI</title>
+  		<title>Book Inventory</title>
   		<link rel="stylesheet" href="css/style.css">
   		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,12 +16,12 @@
 		<header>
 			<div class="header-left">
 				<div class="logo">
-					<img src="imgs/logo.png" alt="Toy R URI Logo">
+					<img src="imgs/book-logo.jpg" alt="Book Inventory Logo">
       			</div>
 
 	      		<nav>
 	      			<ul>
-	      				<li><a href="index.php">Toy Catalog</a></li>
+	      				<li><a href="index.php">Book Catalog</a></li>
 	      				<li><a href="about.php">About</a></li>
 			        </ul>
 			    </nav>
@@ -36,13 +36,13 @@
 
 		<main>
 			<div class="about-container">
-				<h1>About Toys R URI</h1>
+				<h1>About Book Inventory</h1>
 
 				<hr />
 
-				<p>Welcome to Toys R URI, where the fun never ends and the imagination runs wild! At Toys R URI, we believe in the power of play to inspire creativity, ignite curiosity, and bring joy to people of all ages.</p>
-				<p>Our toy store is a magical wonderland filled with a diverse selection of toys, games, and gadgets designed to spark laughter, foster learning, and create unforgettable memories.From classic toys that have stood the test of time to the latest innovations that push the boundaries of imagination, Toys R URI has something for everyone.</p>
-				<p>So come on in, explore our aisles, and let your inner child run free at Toys R URI!</p>
+				<p>Welcome to Book Inventory, where the fun never ends and the imagination runs wild! At Book Inventory, we believe in the power of reading to inspire creativity, ignite curiosity, and bring joy to people of all ages.</p>
+				<p>Our Book Inventory is a magical wonderland filled with a diverse selection of books. You can design various reading Lists for yourself or for a group, great for Book CLubs.</p>
+				<p>So come on in, explore our selection, and let your inner child run free at Book Inventory!</p>
 			</div>
 		</main>
 
