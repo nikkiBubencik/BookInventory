eb46f55af92c8779dcff87ba03a97a98c185c922
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-19 16:23:40 -0400

    Update groups.php

--- groups.php
+++ groups.php
@@ -24,7 +24,7 @@
 		$groups= find_groups_by_name($pdo, $groupName);
 
 		// Check if the list exists
-		if ($groups) {
+		if (!empty($groups)) {
 			// If the list exists, redirect to group.php with groupID parameter
 			header("Location: groups.php?groupName=" . urlencode($groupName));
 			exit(); 
