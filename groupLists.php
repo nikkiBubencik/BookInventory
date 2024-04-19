e88b5c8271fad5e0b7dea87dd3684eef965a8144
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-18 22:18:15 -0400

    Update groupLists.php
    fixed reference to lists.php

--- groupLists.php
+++ groupLists.php
@@ -76,7 +76,7 @@
 
 		    <div class="header-right">
 		    	<ul>
-            <li><a href="groups.php">Groups</a></li>
+            			<li><a href="groups.php">Groups</a></li>
 		    		<li><a href="list.php">Lists</a></li>
 		    	</ul>
 		    </div>
@@ -101,7 +101,7 @@
 				    	<h2><?= $groupName ?>'s Lists</h2>
 				    	<ul>
 				        <?php foreach ($allLists as $list): ?>
-				            <li><a href="list_books.php?listID=<?= $list['listID'] ?>&title=<? $list['list_name'] ?>">
+				            <li><a href="list_books.php?listID=<?= $list['listID'] ?>&title=<?= $list['list_name'] ?>">
 						<?= $list['list_name'] ?></a></li>
 				        <?php endforeach; ?>
 				    	</ul>
