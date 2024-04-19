2541b2f196f54f067b889d9813046a5e88467935
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-18 21:59:36 -0400

    Update list.php
    fixed reference

--- list.php
+++ list.php
@@ -96,7 +96,7 @@
 				    	<h2>Your Lists</h2>
 				    	<ul>
 				        <?php foreach ($allLists as $list): ?>
-				            <li><a href="list_books.php?listID=<?= $list['listID'] ?>&listName=<? $list['list_name'] ?>">
+						<li><a href="list_books.php?listID=<?= $list['listID'] ?>&listName=<?= $list['list_name'] ?>">
 						<?= $list['list_name'] ?></a></li>
 				        <?php endforeach; ?>
 				    	</ul>
