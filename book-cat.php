6ef8145e0dd7bde0650804c6c67a987e3e04b551
mllebexx <135173170+mllebexx@users.noreply.github.com>
2024-04-22 19:38:46 -0400

    Update book-cat.php

--- book-cat.php
+++ book-cat.php
@@ -8,6 +8,7 @@
 		$sql = "SELECT *
 				FROM books
     				WHERE title LIKE :bookName
+				OR authors LIKE :bookName
 				LIMIT 25;";
 		
 		$books = pdo($pdo, $sql, ['bookName' => "%$bookName%"])->fetchAll();		
