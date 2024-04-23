0445fa6f3e9540da33f85f5a85b82a7f9a22af63
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-22 22:54:26 -0400

    Update add-review.php
    echo curdate()

--- add-review.php
+++ add-review.php
@@ -13,7 +13,7 @@
 	    // $stmt = pdo($pdo, $AddReviewSQL, ['bookID' => $bookID, 'review_text' => $review_text, 'userId' => $userId, 'rating' => $rating]); 
 	    // Commit transaction
 	    // $pdo->commit();
-		echo "userid " . $userId ;
+		echo "date " . CURDATE() ;
 	}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
