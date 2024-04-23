d98cbd26f88661c98c161607eedcbf1c7f97d419
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-23 16:41:32 -0400

    Update groups.php
    debugging

--- groups.php
+++ groups.php
@@ -24,11 +24,11 @@
 		// $stmt = pdo($pdo, $sql, ['groupID' => $groupID, 'userId' => $userId]);
 		
 
-		// $memberCountSql = "SELECT count(*) as count FROM user_groups 
-  // 				WHERE groupID = :groupID
-  //     				GROUP BY :groupID;";
-		// $memberCountResult = pdo($pdo, $memberCountSql, ['groupID' => $groupID]);
-		
+		$memberCountSql = "SELECT count(*) as count FROM user_groups 
+  				WHERE groupID = :groupID
+      				GROUP BY :groupID;";
+		$memberCountResult = pdo($pdo, $memberCountSql, ['groupID' => $groupID]);
+		echo " count = " . $memberCountResult['count'];
 		// if($memberCountResult == 0: ){
 		// 	// delete group 
 		// 	$deletGroupSql = "DELETE FROM groups WHERE groupID = :groupID;";
