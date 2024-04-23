00b569197c7618e184b26ee1c5ee81eab6ccfc23
nbubencik <71101143+nbubencik@users.noreply.github.com>
2024-04-22 21:55:13 -0400

    Update add-group-user.php

--- add-group-user.php
+++ add-group-user.php
@@ -29,8 +29,8 @@
 	    }
   
 	    // add user to group
-<!-- 	    $userGroupSql = "INSERT INTO user_groups (groupID, userID) VALUES (:groupId, :userId);";
-	    $stmt = pdo($pdo, $userGroupSql, ['groupId' => $groupId, 'userId' => $userId]); -->
+	    $userGroupSql = "INSERT INTO user_groups (groupID, userID) VALUES (:groupId, :userId);";
+	    $stmt = pdo($pdo, $userGroupSql, ['groupId' => $groupId, 'userId' => $userId]); 
 	
 	    // Commit transaction
 	    $pdo->commit();
