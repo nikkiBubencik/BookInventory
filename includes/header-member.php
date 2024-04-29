e810713f09657a2339e8e5b23733a761875e7dcd
vmelgar9272 <98747760+vmelgar9272@users.noreply.github.com>
2024-04-29 19:57:41 -0400

    Added Account Page to Header

--- includes/header-member.php
+++ includes/header-member.php
@@ -18,17 +18,26 @@
               <nav>
                 <ul>
                   <li><a href="book-cat.php">Book Catalog</a></li>
+                  <li><a href="groups.php">Groups</a></li>
+                  <li><a href="list.php">Lists</a></li>
                   <li><a href="about.php">About</a></li>
-                  <?= $logged_in ? '<a href="logout.php">Log Out</a>' : '<a href="login.php">Log In</a>'; ?>
                 </ul>
-            </nav>
-          </div>
-
-          <div class="header-right">
-            <ul>
-          <li><a href="groups.php">Groups</a></li>
-              <li><a href="list.php">Lists</a></li>
-            </ul>
-          </div>
+              </nav>
+            </div>
+            
+        <div class="header-right">
+        <ul>
+          <?php if ($logged_in) : ?>
+            <li>
+              <div class="welcome">
+                <p>Welcome <?= $_SESSION['username']; ?></p>
+              </div>  
+            </li>
+            <li><a href="account.php">Account</a></li>
+            <li><a href="logout.php">Log Out</a></li>
+            <?php else : ?>
+                <li><a href="login.php">Log In</a></li>
+            <?php endif; ?>
+        </ul>        </div>
       </header>
       <section>
